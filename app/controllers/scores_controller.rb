5712f2c2a0d599b9efd37c00b5380e32f52cea50
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-09-14 16:57:03 -0400

    fixing score creation error

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -16,10 +16,9 @@ class ScoresController < ApplicationController
   # POST /scores
   def create
     @score = Score.new(score_params)
-    byebug
 
     if @score.save
-      render json: @score, status: :created, location: @score
+      render json: @score, status: :created
     else
       render json: @score.errors, status: :unprocessable_entity
     end
